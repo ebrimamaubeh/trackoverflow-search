4eb6d01cf07fe0253f1284342de3661787375e57
Ebrima Jallow <ebrimamaubeh7@gmail.com>
2024-08-21 19:01:51 +0200

    solve copy buttons, but still testing html.

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,97 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const disposable = vscode.commands.registerCommand('trackoverflow-search.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from TrackOverflow-Search!');
+	const commandId = 'trackoverflow-search.mainView';
+	const trackOverflowDisposable = vscode.commands.registerCommand(commandId, () => {
+		
+		const panel = vscode.window.createWebviewPanel(
+			'TrackOverflow Search',
+			'TrackOverflow Search View',
+			vscode.ViewColumn.Two,
+			{
+				enableScripts: true, 
+			}
+		);
+
+        // Get path to resource on disk
+        const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'js/', 'trackoverflow.js');
+        const scriptSrc = panel.webview.asWebviewUri(scriptPath);
+
+		panel.webview.html = getHtmlContent(scriptSrc);
+
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(trackOverflowDisposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+
+function getHtmlContent(scriptSrc: vscode.Uri){
+	return `
+	<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.72.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <div class="container">
+
+        <nav class="navbar navbar-light bg-light sticky-top">
+            <form class="form-inline col-md-12">
+                <input class="form-control form-control-lg" type="search" placeholder="Search StackOverflow" aria-label="Search">
+            </form>
+        </nav>
+
+        <hr>
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  Accordion Item #1
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+
+                    <div class="card" >
+                        <h5 class="card-header">Featured</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk 
+                                of the card's content.
+                            </p>
+                            <a href="#" class="card-link">Card link</a>
+                        </div>
+                    </div>
+
+                </div>
+              </div>
+            </div>
+         
+        </div>
+
+    </div>
+
+    <script src="${scriptSrc}"></script>
+</body>
+
+</html>
+	`;
+}
---
+++ src/js/trackoverflow.js
@@ -0,0 +1,105 @@
+var searchInput = document.getElementById('searchInput');
+searchInput.focus();
+
+
+var searchForm = document.getElementById('searchForm');
+
+searchForm.addEventListener('submit', function(event){
+    //clicking is really a pain with vscode, so don't submit.
+    event.preventDefault();
+
+    getStackOverflowData();
+});
+
+// add copy buttons to text. 
+
+
+async function getStackOverflowData(){
+    // might change this is vscode settings you implement later...
+    const page = 1;
+    const order = 'desc'; // desc, asc...
+    const sort = 'relevance'; // relevance, activity, votes, creation...
+    const pageSize = 20;
+    ////////////////////////////////////////////////////////////////
+
+    // filter using the question using quesiton id.
+    /**
+     * https://api.stackexchange.com/2.3/questions/15182496?order=desc&sort=activity&site=stackoverflow&filter=!*Mg4PjfgUgqOW6wX
+     */
+
+    const searchAPI = 'https://api.stackexchange.com/2.3/search?page='+ page +'&pagesize='+ pageSize +'&order='+ 
+                        order +'&sort='+ sort +'&intitle='+ searchInput.value +'&site=stackoverflow&filter=!*Mg4PjfgUgqOW6wX';
+    let result = await fetch(searchAPI);
+    let jsonData = await result.json();
+    let items = jsonData.items;
+
+    for(let i = 0; i < items.length; i++){
+        console.log(items[i]);
+        createResultHTML(items[i], i);
+    }
+
+    var codes = document.querySelectorAll('pre > code');
+    for(let i = 0; i < codes.length; i++){
+        codes[i].parentNode.after(getCopyButton()); // not working yet.
+    }
+
+}
+
+function getCopyButton(){
+    var button = document.createElement('button');
+    button.setAttribute('class', 'btn btn-primary');
+    button.innerHTML = 'Copy Code';
+
+    return button;
+}
+
+function addCodeStyles(){
+    var pre = document.querySelectorAll('pre');
+    for(let i = 0; i < pre.length; i++){
+        pre[i].style.backgroundColor = 'lightGrey';
+    }
+
+}
+
+function createResultHTML(question, counter){
+
+    var dataTarget = 'flush-collapse' + counter;
+    var result = renderQuestionHTML(question.title, question.body, dataTarget);
+
+    var accordionDiv = document.getElementById('accordionFlushDiv');
+
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = result;
+    accordionDiv.appendChild(newDiv);
+
+    addCodeStyles(); // add some styles.
+    
+}
+
+function renderQuestionHTML(title, text, dataTarget){
+    return `
+         <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed bg-info-subtle" type="button" data-bs-toggle="collapse" data-bs-target="#`+ dataTarget +`" aria-expanded="false" aria-controls="flush-collapseOne">
+                  `+ title +`
+                </button>
+              </h2>
+              <div id="`+ dataTarget +`" class="accordion-collapse collapse" data-bs-parent="#accordionFlushDiv">
+                <div class="accordion-body">
+
+                    <div class="card" >
+                        <h5 class="card-header">Question</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">`+ title +`</h5>
+                            <p class="card-text">
+                                `+ text +`
+                            </p>
+                            <a href="#" class="card-link">Card link</a>
+                        </div>
+                    </div>
+
+                </div>
+              </div>
+            </div>
+`;
+}
