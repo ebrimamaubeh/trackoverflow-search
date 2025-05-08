8f47f398631583c52f7bc2a81af253f343c7c550
Ebrima Jallow <ebrimamaubeh7@gmail.com>
2025-05-08 14:34:38 +0200

    pasting is tracked. user copies code, then program tracks if text is pasted. todo need to check if its same text

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { TrackOverflowPost, createTrackOverflowPost } from './trackOverflowData';
+import { TrackOverflowPost } from './trackOverflowData';
 import * as Helpers from './helpers';
 
 // Global variable to store the interval ID
 let intervalId: NodeJS.Timeout | undefined;
-let storagePanelVisible : boolean = false;
+let storagePanelVisible: boolean = false;
+
+// track if user copied code.
+let hasCopiedCode: boolean = false;
+let tempCopiedPost: any = null;
+const PAST_THRESHOLD = 10;   // typing 10 chars instantly means pasting...
+// track if user copied code.
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,15 +22,15 @@ export function activate(context: vscode.ExtensionContext) {
     // Create a background task that runs every 5 munites.
     const ONE_SECOND = 1000;
     const ONE_HOUR = ONE_SECOND * 60 * 60;
-    intervalId = setInterval( async () => {
+    intervalId = setInterval(async () => {
 
         const hasUnseenPost = Helpers.hasUnseenPost(context);
         const hasPostBeenUpdated = await Helpers.hasPostBeenUpdated(context);
         const hasNewComments = await Helpers.postsHasNewComments(context);
-        if(hasUnseenPost && (hasPostBeenUpdated || hasNewComments) && !storagePanelVisible){
+        if (hasUnseenPost && (hasPostBeenUpdated || hasNewComments) && !storagePanelVisible) {
             //TODO: get current panel. dont show this message if you are on dataStorage page.
             const warningMessage = 'Warning: Some Code You Copied Has Changed';
-            const selection = await vscode.window.showWarningMessage(warningMessage,'Show List', 'Ignore');
+            const selection = await vscode.window.showWarningMessage(warningMessage, 'Show List', 'Ignore');
             if ((selection !== undefined) && selection !== 'Ignore') {
                 // check how to pass args to commands.
                 vscode.commands.executeCommand('trackoverflow-search.dataStorage', true);
@@ -50,55 +56,96 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Helpers.getNewRevisionsPost(context); // implement this funciton in html.
 
-	const commandId = 'trackoverflow-search.mainView';
-	const trackOverflowDisposable = vscode.commands.registerCommand(commandId, async () => {
-		
-		const panel = vscode.window.createWebviewPanel(
-			'TrackOverflow Search',
-			'TrackOverflow Search View',
-			vscode.ViewColumn.Two,
-			{
-				enableScripts: true, 
+    const commandId = 'trackoverflow-search.mainView';
+    const trackOverflowDisposable = vscode.commands.registerCommand(commandId, async () => {
+
+        const panel = vscode.window.createWebviewPanel(
+            'TrackOverflow Search',
+            'TrackOverflow Search View',
+            vscode.ViewColumn.Two,
+            {
+                enableScripts: true,
                 retainContextWhenHidden: true, // save when switch from one window to the next.
-			}
-		);
+            }
+        );
 
         // Get path to resource on disk
         const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'src/js/', 'trackoverflow.js');
         const scriptSrc = panel.webview.asWebviewUri(scriptPath);
 
-		panel.webview.html = getHtmlContent(scriptSrc);
-           
+        panel.webview.html = getHtmlContent(scriptSrc);
+
         // Handle messages from the webview
         panel.webview.onDidReceiveMessage(
             message => {
-                switch(message.command){
-                    case 'copy': 
-                        const post: TrackOverflowPost = createTrackOverflowPost(message);
-                        const key = post.id.toString();
-                        context.workspaceState.update(key, post);
-                        vscode.window.showInformationMessage('copied');
-                    break;
+                switch (message.command) {
+                    case 'copy':
+                        tempCopiedPost = new TrackOverflowPost(message.id, message.dateCopied, message.lastEdited, message.code, message.post, message.link, false);
+                        // const key = post.id.toString();
+                        hasCopiedCode = true;
+                        //context.workspaceState.update(key, post);
+                        showMessageT('copied');
+                        break;
                 }
             },
             undefined,
             context.subscriptions
         );
 
-	});
+        // handle pasting events. 
+        vscode.workspace.onDidChangeTextDocument(event => {
+            // Call your function to analyze the document changes
+            handleTextDocumentChange(event, context);
+        });
 
+    });
+
+    function handleTextDocumentChange(event: vscode.TextDocumentChangeEvent, context: vscode.ExtensionContext) {
+        if (hasCopiedCode) {
+            if (event.contentChanges.length > 0) {
+                const change = event.contentChanges[0]; // Typically, a paste results in a single large change
+
+                const hasPasted = change.rangeLength === 0 && change.text.length > PAST_THRESHOLD;
+                const hasReplacedPasted = change.rangeLength > 0 && change.text.length > change.rangeLength * 2;
+
+                if ((hasPasted || hasReplacedPasted) && tempCopiedPost) {
+                    const key = tempCopiedPost.id.toString();
+                    // if the paste happend, then you should store the workspace obj.
+                    context.workspaceState.update(key, tempCopiedPost);
+
+                    showMessageT('Pasted Code is being Tracked.');
+
+                    // reset variables. 
+                    hasCopiedCode = false;
+                    tempCopiedPost = null;
+                }
+            }
+        }
+    }
+
+    // function to show message with a timeout.
+    function showMessageT(message: string) {
+
+        // got this code snippet from gemeni. 
+        const output = vscode.window.showInformationMessage(message);
+        setTimeout(() => {
+            output.then((value) => { // value not needed for disposal. 
+                (output as any)._disposable.dispose();
+            });
+        }, 1000); // = 1 second.
+    }
 
     const trackOverflowStorageDisposable = vscode.commands.registerCommand('trackoverflow-search.dataStorage', async (setIntervalArg) => {
-        
+
         const panel = vscode.window.createWebviewPanel(
-			'TrackOverflow Search',
-			'TrackOverflow Stored Data',
-			vscode.ViewColumn.Two,
-			{
-				enableScripts: true, 
+            'TrackOverflow Search',
+            'TrackOverflow Stored Data',
+            vscode.ViewColumn.Two,
+            {
+                enableScripts: true,
                 retainContextWhenHidden: true, // save when switch from one window to the next.
-			}
-		);
+            }
+        );
 
 
         const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'src/js/', 'dataStorage.js');
@@ -113,9 +160,9 @@ export function activate(context: vscode.ExtensionContext) {
         //testing...
         panel.onDidDispose(() => { storagePanelVisible = false; }, null, context.subscriptions);
         //testing...
-        
+
         //TODO: This should change later, to revisions or comments. 
-        if(setIntervalArg && Helpers.unSeenPostCount(updated_posts) === 1){ // function called by setInterval...
+        if (setIntervalArg && Helpers.unSeenPostCount(updated_posts) === 1) { // function called by setInterval...
             var post = updated_posts[0];//only one post.
             panel.webview.postMessage({
                 command: 'detail-revision-post',//change the if to down.
@@ -123,30 +170,30 @@ export function activate(context: vscode.ExtensionContext) {
                 post: post
             });
         }
-        else{
+        else {
             /**
              * TODO: remove if and else. change with
              *  if(newcomments){links for new comments.}
              * else{ new post links }
              */
             //TODO: this should have both post revisions or comments. 
-            
+
             const postHasNewComments = await Helpers.postsHasNewComments(context);
             const postHasNewRevisions = await Helpers.hasNewRevisionsPost(context);
 
-            if(postHasNewRevisions || postHasNewComments){ // bring revisions.
+            if (postHasNewRevisions || postHasNewComments) { // bring revisions.
                 const revisions_with_snippets = await Helpers.getNewRevisionsPost(context);
                 const comments_with_snippets = await Helpers.getCommentsWithWordShippets(context);
                 //await Helpers.getStoredDataPostIDs(context);
                 const post_ids = context.workspaceState.keys();
                 console.log('post ids here: ', post_ids);
-                
+
                 // revisions or comments can be missing.
                 panel.webview.postMessage({
-                    command: 'list-post-revisions-comments', 
+                    command: 'list-post-revisions-comments',
                     revisions_with_snippets: revisions_with_snippets,
                     comments_with_snippets: comments_with_snippets,
-                    post_ids: post_ids, 
+                    post_ids: post_ids,
 
                 });
             }
@@ -158,58 +205,58 @@ export function activate(context: vscode.ExtensionContext) {
             switch (message.command) {
                 case 'dataStorage-detail-page':
                     var post = context.workspaceState.get(message.post_id);
-                    panel.webview.postMessage({ 
+                    panel.webview.postMessage({
                         command: 'detail-revision-post',
                         post_id: message.post_id, //here...
                         post: post
                     });
-                break;
-                case 'back-button': 
+                    break;
+                case 'back-button':
                     const post_revisions = await Helpers.getNewRevisionsPost(context);
                     const comments_with_snippets = await Helpers.getCommentsWithWordShippets(context);
-                    panel.webview.postMessage({ 
+                    panel.webview.postMessage({
                         command: 'list-post-revisions-comments',
                         post_revisions: post_revisions,
                         comments_with_snippets: comments_with_snippets
                     });
-                break;
-                case 'hide-button': 
+                    break;
+                case 'hide-button':
                     // hide post should just delete post.
                     const warningMessage = 'This Will Delete Revision. Are You Sure?';
-                    const selection = await vscode.window.showWarningMessage(warningMessage,'Delete?', 'Ignore');
+                    const selection = await vscode.window.showWarningMessage(warningMessage, 'Delete?', 'Ignore');
                     if ((selection !== undefined) && selection !== 'Ignore') {
                         var oldPost: TrackOverflowPost | undefined = context.workspaceState.get(message.post_id);
-                        if(oldPost){
+                        if (oldPost) {
                             context.workspaceState.update(oldPost.id.toString(), undefined); // delete..
                         }
                     }
 
                     // then send him to main page.
                     var updated_posts = await Helpers.getAllUpdatedStoredPosts(context);
-                    panel.webview.postMessage({ 
+                    panel.webview.postMessage({
                         command: 'list-posts',
-                        updated_posts: updated_posts 
+                        updated_posts: updated_posts
                     });
-                break;
-                case 'update-seen': 
+                    break;
+                case 'update-seen':
                     var oldPost: TrackOverflowPost | undefined = context.workspaceState.get(message.post.id);
-                    if(oldPost){
-                        if( !oldPost.seen ){
+                    if (oldPost) {
+                        if (!oldPost.seen) {
                             oldPost.seen = true;
                             context.workspaceState.update(oldPost.id.toString(), oldPost);
                         }
                     }
-                    else{
-                        throw new Error('Cannot update Post to Seen: '+ oldPost);
-                    }       
-                break;
+                    else {
+                        throw new Error('Cannot update Post to Seen: ' + oldPost);
+                    }
+                    break;
 
             }
         });
 
     });
 
-	context.subscriptions.push(trackOverflowDisposable);
+    context.subscriptions.push(trackOverflowDisposable);
     context.subscriptions.push(trackOverflowStorageDisposable);
 }
 
@@ -220,7 +267,7 @@ export function deactivate() {
 }
 
 
-function getDataPageHTML(scriptSrc: vscode.Uri){
+function getDataPageHTML(scriptSrc: vscode.Uri) {
     return `
         <!DOCTYPE html>
         <html>
@@ -283,11 +330,11 @@ function getDataPageHTML(scriptSrc: vscode.Uri){
         </body>
         </html>
     `;
-}   
+}
 
 // this code is from index.js
-function getHtmlContent(scriptSrc: vscode.Uri){
-	return `
+function getHtmlContent(scriptSrc: vscode.Uri) {
+    return `
         <!DOCTYPE html>
             <html>
             <head>
--- src/trackOverflowData.ts
+++ src/trackOverflowData.ts
@@ -1,36 +1,57 @@
+import { deactivate } from './extension';
 
 // the interface...
-export interface TrackOverflowPost{
+// export interface TrackOverflowPost {
+//     id: number; // copied post id.
+//     dateCopied: Date;
+//     lastEdited: Date;
+//     code: string;
+//     post: string;
+//     link: string;
+//     seen: boolean; // post has been seen.
+// }
+
+export class TrackOverflowPost {
     id: number; // copied post id.
     dateCopied: Date;
     lastEdited: Date;
     code: string;
-    post: string;
+    post: string; // actual post on StackOverflow.
     link: string;
     seen: boolean; // post has been seen.
+
+    constructor(id: number, dateCopied: Date, lastEdited: Date, code: string, post: string, link: string, seen: boolean) {
+        this.id = id;
+        this.dateCopied = dateCopied;
+        this.lastEdited = lastEdited;
+        this.code = code;
+        this.post = post;
+        this.link = link;
+        this.seen = seen;
+    }
 }
 
-export function createTrackOverflowPost(message: any){
-    const post: TrackOverflowPost = {
-        id: message.id,
-        dateCopied: message.dateCopied,
-        lastEdited: message.lastEdited,
-        code: message.code,
-        post: message.post,
-        link: message.link,
-        seen: false, 
-    }; 
+// export function createTrackOverflowPost(message: any) {
+//     const post: TrackOverflowPost = {
+//         id: message.id,
+//         dateCopied: message.dateCopied,
+//         lastEdited: message.lastEdited,
+//         code: message.code,
+//         post: message.post,
+//         link: message.link,
+//         seen: false,
+//     };
 
-    return post;
-}
+//     return post;
+// }
 
 // a set of words I will be looking for in edits and updates.
-export function wordSnippetList(){
+export function wordSnippetList() {
     // link to get comments. 
     // https://api.stackexchange.com/docs/comments-on-posts
     const array = [
-        'error', 'errors','debug error', 
-        'code does not work', 'contains errors', 'not working', 
+        'error', 'errors', 'debug error',
+        'code does not work', 'contains errors', 'not working',
         'deprecated code', 'update', 'updated code', 'code upgrade', 'code changed',
         'more details'
     ];
